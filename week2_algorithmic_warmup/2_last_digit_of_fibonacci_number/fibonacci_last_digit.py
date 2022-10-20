3778223a60a662583c9b16abd44a8bb000106254
Abdul6498 <abdulrehman6498@outlook.com>
2022-10-21 00:59:53 +0200

    no change

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -11,6 +11,18 @@ def fibonacci_last_digit(n):
     return current % 10
 
 
+def fibonacci_last_digit_fast(n):
+    if n <= 1:
+        return n
+    previous = 0
+    current  = 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current)%10
+
+    return current
+
+
 if __name__ == '__main__':
     n = int(input())
     print(fibonacci_last_digit(n))
+    print(fibonacci_last_digit_fast(n))
